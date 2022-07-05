1500df7ee3894014f101958116b35a27b07d372b
andrewj31415 <andrewj31415@gmail.com>
2022-07-05 13:19:58 -0400

    started placing multiple modules

--- visual/draw.py
+++ visual/draw.py
@@ -57,6 +57,14 @@ class Module(Component):
             node.translate(dx, dy)
         for node in self.outputs:
             node.translate(dx, dy)
+    def autoPlace(self):
+        '''automatically place nodes'''
+        for i in range(len(self.inputs)):
+            self.inputs[i].x = self.x
+            self.inputs[i].y = self.height*(i+1)/(len(self.inputs)+1)
+        for i in range(len(self.outputs)):
+            self.outputs[i].x = self.x + self.width
+            self.outputs[i].y = self.height*(i+1)/(len(self.outputs)+1)
     def toJavaScript(self):
         d = { 'name': "`" + self.name + "`", 'type': "`" + self.type + "`", 'variant': "'Module'", 'source': [],
                 'typeSource': [], 'x': self.x, 'y': self.y, 'width': self.width, 'height': self.height,
@@ -82,6 +90,13 @@ class Function(Component):
         for node in self.inputs:
             node.translate(dx, dy)
         self.output.translate(dx, dy)
+    def autoPlace(self):
+        '''automatically place nodes'''
+        for i in range(len(self.inputs)):
+            self.inputs[i].x = self.x
+            self.inputs[i].y = self.height*(i+1)/(len(self.inputs)+1)
+        self.output.x = self.x + self.width
+        self.output.y = self.y + self.height/2
     def toJavaScript(self):
         d = { 'name': "`" + self.name + "`", 'variant': "'Function'", 'source': '[]',
                 'typeSource': '[]', 'x': self.x, 'y': self.y, 'width': self.width, 'height': self.height,
@@ -101,6 +116,12 @@ class Register(Component):
         self.y += dy
         self.input.translate(dx, dy)
         self.output.translate(dx, dy)
+    def autoPlace(self):
+        '''automatically place nodes'''
+        self.input.x = self.x
+        self.input.y = self.y + self.height/2
+        self.output.x = self.x + self.width
+        self.output.y = self.y + self.height/2
     def toJavaScript(self):
         d = { 'name': "`" + self.name + "`", 'type': "`" + self.type + "`", 'variant': "'Register'", 'source': [],
                 'typeSource': [], 'x': self.x, 'y': self.y, 'width': self.width, 'height': self.height }
@@ -121,6 +142,15 @@ class Mux(Component):
             node.translate(dx, dy)
         self.output.translate(dx, dy)
         self.control.translate(dx, dy)
+    def autoPlace(self):
+        '''automatically place nodes'''
+        for i in range(len(self.inputs)):
+            self.inputs[i].x = self.x
+            self.inputs[i].y = self.height*(i+1)/(len(self.inputs)+1)
+        self.output.x = self.x + self.width
+        self.output.y = self.y + self.height/2
+        self.control.x = self.x + self.width/2
+        self.control.y = self.height - (self.height - self.shortHeight)/4
     def toJavaScript(self):
         d = { 'variant': "'Mux'", 'source': [],
                 'x': self.x, 'y': self.y, 'width': self.width, 'height': self.height, 'shortHeight': self.shortHeight }
@@ -156,39 +186,34 @@ class Wire(Component):
         return '{' + ", ".join( key+": "+str(d[key]) for key in d) + '}'
 
 
-uWidth, uHeight = 100, 60
-uMuxWidth, uMuxHeight = 3, 10 #Height of the longer side
-uAdderWidth, uAdderHeight = 10, 10
-uConst1Width, uConst1Height = 10, 5
-uRegWidth, uRegHeight = 10, 20
+ulWidth, ulHeight = 100, 60
+ulMuxWidth, ulMuxHeight = 3, 10 #Height of the longer side
+ulMuxShortHeight = ulMuxHeight*5/8 #Height of the shorter side
+ulAdderWidth, ulAdderHeight = 10, 10
+ulConst1Width, ulConst1Height = 10, 5
+ulRegWidth, ulRegHeight = 10, 20
 
 #upper left corner Coords
-uMuxC = [3*uWidth/5, uHeight/2 - uMuxHeight/2]
-uRegC = [4*uWidth/5, uHeight/2 - uRegHeight/2]
-uConst1C = [uWidth/5, 3*uHeight/4 - uConst1Height/2]
-uAdderC = [2*uWidth/5, 5*uHeight/8 - uAdderHeight/2]
+ulMuxC = [3*ulWidth/5 - ulMuxWidth/2, ulHeight/2 - ulMuxHeight/2]
+ulRegC = [4*ulWidth/5 - ulRegWidth/2, ulHeight/2 - ulRegHeight/2]
+ulConst1C = [ulWidth/5 - ulConst1Width/2, 3*ulHeight/4 - ulConst1Height/2]
+ulAdderC = [2*ulWidth/5 - ulAdderWidth/2, 5*ulHeight/8 - ulAdderHeight/2]
 
 uMux = Mux("uMux", [Node("m1"), Node("m2")])
-uMux.x, uMux.y, uMux.width, uMux.height, uMux.shortHeight = 0, 0, uMuxWidth, uMuxHeight, uMuxHeight*5/8
-uMux.inputs[0].placeAt(0, uMuxHeight/3)
-uMux.inputs[1].placeAt(0, 2*uMuxHeight/3)
-uMux.output.placeAt(uMuxWidth, uMuxHeight/2)
-uMux.control.placeAt(uMuxWidth/2, uMuxHeight - (uMuxHeight-uMux.shortHeight)/4)
+uMux.x, uMux.y, uMux.width, uMux.height, uMux.shortHeight = 0, 0, ulMuxWidth, ulMuxHeight, ulMuxShortHeight
+uMux.autoPlace()
 
 uAdder = Function("+", [], [Node("a1"), Node("a2")])
-uAdder.x, uAdder.y, uAdder.width, uAdder.height = 0, 0, uAdderWidth, uAdderHeight
-uAdder.inputs[0].placeAt(0, uAdderHeight/3)
-uAdder.inputs[1].placeAt(0, 2*uAdderHeight/3)
-uAdder.output.placeAt(uAdderWidth, uAdderHeight/2)
+uAdder.x, uAdder.y, uAdder.width, uAdder.height = 0, 0, ulAdderWidth, ulAdderHeight
+uAdder.autoPlace()
 
 uConst1 = Function("1'b1", [], [])
-uConst1.x, uConst1.y, uConst1.width, uConst1.height = 0, 0, uConst1Width, uConst1Height
-uConst1.output.placeAt(uConst1Width, uConst1Height/2)
+uConst1.x, uConst1.y, uConst1.width, uConst1.height = 0, 0, ulConst1Width, ulConst1Height
+uConst1.autoPlace()
 
 uReg = Register("count", "Bit#(2)")
-uReg.x, uReg.y, uReg.width, uReg.height = 0, 0, uRegWidth, uRegHeight
-uReg.input.placeAt(0, uRegHeight/2)
-uReg.output.placeAt(uRegWidth, uRegHeight/2)
+uReg.x, uReg.y, uReg.width, uReg.height = 0, 0, ulRegWidth, ulRegHeight
+uReg.autoPlace()
 
 u = Module('upper', 'TwoBitCounter', [uMux, uAdder, uConst1, uReg], [Node("enable")], [Node("getCount")])
 u.children.extend([ Wire(u.inputs[0], uMux.control), Wire(uMux.output, uReg.input),
@@ -196,40 +221,90 @@ u.children.extend([ Wire(u.inputs[0], uMux.control), Wire(uMux.output, uReg.inpu
                     Wire(uConst1.output, uAdder.inputs[1]), Wire(uReg.output, uMux.inputs[0]),
                     Wire(uAdder.output, uMux.inputs[1]) ]) #add wires to u
 
-u.x, u.y, u.width, u.height = 0, 0, uWidth, uHeight
-u.inputs[0].placeAt(0, uHeight/2)
-u.outputs[0].placeAt(uWidth, uHeight/2)
+u.x, u.y, u.width, u.height = 0, 0, ulWidth, ulHeight
+u.autoPlace()
 
-uAdder.translate(*uAdderC)
-uConst1.translate(*uConst1C)
-uMux.translate(*uMuxC)
-uReg.translate(*uRegC)
+uAdder.translate(*ulAdderC)
+uConst1.translate(*ulConst1C)
+uMux.translate(*ulMuxC)
+uReg.translate(*ulRegC)
 
-u.children[4].placeAt(uMuxC[0]/8, 7*uHeight/8, uMuxC[0] + uMuxWidth/2)
+u.children[4].placeAt(ulMuxC[0]/8, 7*ulHeight/8, ulMuxC[0] + ulMuxWidth/2)
 u.children[5].autoPlace()
 u.children[6].autoPlace()
-u.children[7].placeAt(uRegC[0] + uRegWidth + 5, uHeight/6, uAdderC[0] - 10)
+u.children[7].placeAt(ulRegC[0] + ulRegWidth + 5, ulHeight/6, ulAdderC[0] - 10)
 u.children[8].autoPlace()
-u.children[9].placeAt(uRegC[0] + uRegWidth + 5, uHeight/6, uMuxC[0] - 10)
+u.children[9].placeAt(ulRegC[0] + ulRegWidth + 5, ulHeight/6, ulMuxC[0] - 10)
 u.children[10].autoPlace()
 
+# organize l
+lMux = Mux("lMux", [Node("m1"), Node("m2")])
+lMux.x, lMux.y, lMux.width, lMux.height, lMux.shortHeight = 0, 0, ulMuxWidth, ulMuxHeight, ulMuxShortHeight
+lMux.inputs[0].placeAt(0, ulMuxHeight/3)
+lMux.inputs[1].placeAt(0, 2*ulMuxHeight/3)
+lMux.output.placeAt(ulMuxWidth, ulMuxHeight/2)
+lMux.control.placeAt(ulMuxWidth/2, ulMuxHeight - (ulMuxHeight-ulMuxShortHeight)/4)
 
-print(u)
-print(u.toJavaScript())
-
-'''lMux = Mux("uMux", [Node("m1"), Node("m2")])
 lAdder = Function("+", [], [Node("a1"), Node("a2")])
-lConst1 = Function("1b'1", [], [])
-lReg = Register("count")
+lAdder.x, lAdder.y, lAdder.width, lAdder.height = 0, 0, ulAdderWidth, ulAdderHeight
+lAdder.inputs[0].placeAt(0, ulAdderHeight/3)
+lAdder.inputs[1].placeAt(0, 2*ulAdderHeight/3)
+lAdder.output.placeAt(ulAdderWidth, ulAdderHeight/2)
 
-l = Module('TwoBitCounter', [lMux, lAdder, lConst1, lReg], [Node("enable")], [Node("getCount")])
+lConst1 = Function("1'b1", [], [])
+lConst1.x, lConst1.y, lConst1.width, lConst1.height = 0, 0, ulConst1Width, ulConst1Height
+lConst1.output.placeAt(ulConst1Width, ulConst1Height/2)
+
+lReg = Register("count", "Bit#(2)")
+lReg.x, lReg.y, lReg.width, lReg.height = 0, 0, ulRegWidth, ulRegHeight
+lReg.input.placeAt(0, ulRegHeight/2)
+lReg.output.placeAt(ulRegWidth, ulRegHeight/2)
+
+l = Module('lower', 'TwoBitCounter', [lMux, lAdder, lConst1, lReg], [Node("enable")], [Node("getCount")])
 l.children.extend([ Wire(l.inputs[0], lMux.control), Wire(lMux.output, lReg.input),
                     Wire(lReg.output, l.outputs[0]), Wire(lReg.output, lAdder.inputs[0]),
                     Wire(lConst1.output, lAdder.inputs[1]), Wire(lReg.output, lMux.inputs[0]),
                     Wire(lAdder.output, lMux.inputs[1]) ]) #add wires to l
 
-m = Module('FourBitCounter', [u, l])'''
+l.x, l.y, l.width, l.height = 0, 0, ulWidth, ulHeight
+l.inputs[0].placeAt(0, ulHeight/2)
+l.outputs[0].placeAt(ulWidth, ulHeight/2)
 
+lAdder.translate(*ulAdderC)
+lConst1.translate(*ulConst1C)
+lMux.translate(*ulMuxC)
+lReg.translate(*ulRegC)
+
+l.children[4].placeAt(ulMuxC[0]/8, 7*ulHeight/8, ulMuxC[0] + ulMuxWidth/2)
+l.children[5].autoPlace()
+l.children[6].autoPlace()
+l.children[7].placeAt(ulRegC[0] + ulRegWidth + 5, ulHeight/6, ulAdderC[0] - 10)
+l.children[8].autoPlace()
+l.children[9].placeAt(ulRegC[0] + ulRegWidth + 5, ulHeight/6, ulMuxC[0] - 10)
+l.children[10].autoPlace()
+
+mWidth, mHeight = 200, 200
+mConcatWidth, mConcatHeight = 10, 10
+
+uC = [30, 20]
+lC = [10, 120]
+mConcatC = [150, 100]
+
+mConcat = Function("{}", [], [Node("a1"), Node("a2")])
+mConcat.x, mConcat.y, mConcat.width, mConcat.height = 0, 0, mConcatWidth, mConcatHeight
+mConcat.autoPlace()
+
+m = Module('stuff', 'FourBitCounter', [u, l, mConcat], [Node("enable")], [Node("getCount")])
+
+m.x, m.y, m.width, m.height = 0, 0, mWidth, mHeight
+m.autoPlace()
+
+u.translate(*uC)
+l.translate(*lC)
+mConcat.translate(*mConcatC)
+
+print(m)
+print(m.toJavaScript())
 
 # Graph drawing algorithms:
 # https://en.wikipedia.org/wiki/Layered_graph_drawing
@@ -240,7 +315,7 @@ templateFile = pathlib.Path(__file__).with_name('template.html')
 
 template = templateFile.read_text()
 
-template = u.toJavaScript().join(template.split("/* Python elements go here */"))
+template = m.toJavaScript().join(template.split("/* Python elements go here */"))
 
 output = pathlib.Path(__file__).with_name('sample.html')
 output.open("w").write(template)
